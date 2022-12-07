5b066696a75cbc48d96c4fb570931e7b2c53bccf
Gly <gabin.lefranc@gmail.com>
2022-12-07 14:34:39 +0100

    simple build tree

--- include/cstfmt/parser.h
+++ include/cstfmt/parser.h
@@ -1,76 +1,134 @@
+#include <_types/_uint8_t.h>
 #include <cstddef>
 #include <string_view>
+#include <string>
 #include <optional>
 #include "strlit.h"
 
-struct SpanPosition {
-    ssize_t begin;
-    ssize_t end;
+struct PlainText {
+    std::string text;
 };
-template <SpanPosition span_pos>
-struct SpanString : strlit::details::BaseString<span_pos.end-span_pos.begin> {
-    template <size_t N>
-    constexpr SpanString(const char (&str)[N]) : strlit::details::BaseString<span_pos.end-span_pos.begin>() {
-        std::copy(str+span_pos.begin, str+span_pos.end, this->text);
-    }
-    static constexpr SpanPosition position = span_pos;
+struct Argument {
+    uint8_t index = -1;
+    std::vector<std::variant<PlainText, Argument>> format;
 };
 
 struct Lexer {
-    enum class TokenType {
-        PlainText,
-        Argument,
-        None
+    struct Character {
+        char c;
+        size_t pos;
+        bool escaped = false;
     };
-    struct Token {
-        size_t begin, end;
-        TokenType type;
-        constexpr size_t length() const { 
-            return end-begin; 
-        }
-    };
-    constexpr Lexer(std::string_view str, size_t pos = 0) : next_type(TokenType::PlainText), pos(pos), text(str) 
+    constexpr Lexer(std::string_view str, size_t pos = 0) : pos(pos), text(str) 
     {}
+    constexpr size_t to_int(std::string_view str) const {
+        size_t result = 0;
+        for (auto c : str) {
+            result = result*10 + (c-'0');
+        }
+        return result;
+    }
 
-    constexpr std::optional<Token> next() {
+    constexpr std::optional<Character> next_character() {
         if (pos >= text.length()) {
             return std::nullopt;
         }
-        auto prev_pos = this->pos;
-        while(this->pos < text.length()) {
-            switch(this->text[this->pos]) {
+        while (pos < text.length()) {
+            switch(text[pos]) {
                 case '{':
                 {
-                    auto current_type = this->next_type;
-                    if (this->next_type == TokenType::PlainText && this->pos != text.length()-1 && this->text[this->pos+1] == '{') {
-                        auto current_pos = this->pos+1;
-                        this->pos += 2;
-                        return Token{prev_pos, current_pos, current_type};
-                    }
-                    this->next_type = TokenType::Argument;
-                    return Token{prev_pos, this->pos++, current_type};
+                    // if (pos != text.length()-1 && text[pos+1] == '{') {
+                    //     pos += 2;
+                    //     return Character{'{', pos-2, true};
+                    // }
+                    return Character{'{', pos++, false};
                 }
                 case '}':
                 {
-                    auto current_type = this->next_type;
-                    if (this->next_type == TokenType::PlainText && this->pos != text.length()-1 && this->text[this->pos+1] == '}') {
-                        auto current_pos = this->pos+1;
-                        this->pos += 2;
-                        return Token{prev_pos, current_pos, current_type};
-                    }
-                    this->next_type = TokenType::PlainText;
-                    return Token{prev_pos, this->pos++, current_type};
+                    // if (pos != text.length()-1 && text[pos+1] == '}') {
+                    //     pos += 2;
+                    //     return Character{'}', pos-2, true};
+                    // }
+                    return Character{'}', pos++, false};
                 }
+                case ':':
+                    return Character{':', pos++, false};
                 default:
-                    this->pos++;
+                    pos++;
             }
         }
-        return Token{prev_pos, this->pos, this->next_type};
+        return std::nullopt;
     }
-    TokenType next_type;
+    constexpr auto build_tree() {
+        auto lexer = Lexer(text);
+        std::vector<std::variant<PlainText, Argument>> result;
+        std::vector<Argument> stack_arguments;
+        auto prev_pos = 0;
+        auto add_string_to_result = [&](auto&& str) {
+            if (!str.empty()) {
+                if (stack_arguments.empty()) {
+                    result.push_back(PlainText{std::string(str)});
+                } else {
+                    stack_arguments.back().format.push_back(PlainText{std::string(str)});
+                }
+            }
+        };
+        auto get_index = [&](auto pos) {
+            if (prev_pos != pos) {
+                return to_int(lexer.text.substr(prev_pos, pos-prev_pos));
+            } else {
+                return current_index++;
+            }
+        };
+        while (auto ch = next_character()) {
+            if (ch->c == '{') {
+                add_string_to_result(lexer.text.substr(prev_pos, ch->pos-prev_pos));
+                stack_arguments.push_back(Argument{});
+            } else if (ch->c == '}') {
+                // if (stack_arguments.empty()) {
+                //     throw std::runtime_error("Unexpected '}'");
+                // }
+                if (stack_arguments.back().index == static_cast<uint8_t>(-1)) {
+                    stack_arguments.back().index = get_index(ch->pos);
+                } else {
+                    add_string_to_result(lexer.text.substr(prev_pos, ch->pos-prev_pos));
+                }
+                auto arg = stack_arguments.back();
+                stack_arguments.pop_back();
+                if (stack_arguments.empty()) {
+                    result.push_back(arg);
+                } else {
+                    stack_arguments.back().format.push_back(arg);
+                }
+            } else if (ch->c == ':') {
+                // if (stack_arguments.empty()) {
+                //     throw std::runtime_error("Unexpected ':'");
+                // }
+                // if (stack_arguments.back().index != 0) {
+                //     throw std::runtime_error("Unexpected ':'");
+                // }
+                stack_arguments.back().index = get_index(ch->pos);
+            }
+            prev_pos = ch->pos+1;
+        }
+        add_string_to_result(lexer.text.substr(prev_pos, lexer.pos-prev_pos));
+        // if (!stack_arguments.empty()) {
+        //     throw std::runtime_error("Missing '}'");
+        // }
+        return result;
+    }
+    constexpr void reset() {
+        this->pos = 0;
+        this->current_index = 0;
+    }
     size_t pos = 0;
+    size_t current_index = 0;
     std::string_view text;
+    // TokenType current_type;
+    // std::vector<PartialToken> partial_tokens;
 };
+#if 0
+
 // template <size_t N>
 struct Parser {
     constexpr Parser(std::string_view str) : text(str)
@@ -78,7 +136,7 @@ struct Parser {
     constexpr size_t length_plaintext() const {
         auto lexer = Lexer(text);
         size_t len = 0;
-        while(auto token = lexer.next()) {
+        while(auto token = lexer.next_character()) {
             if (token->type == Lexer::TokenType::PlainText) {
                 len += token->length();
             }
@@ -88,7 +146,7 @@ struct Parser {
     constexpr size_t number_arguments() const {
         auto lexer = Lexer(text);
         size_t num = 0;
-        while(auto token = lexer.next()) {
+        while(auto token = lexer.next_character()) {
             if (token->type == Lexer::TokenType::Argument) {
                 num++;
             }
@@ -104,7 +162,7 @@ struct Format : strlit::details::BaseString<Parser{to_parse}.length_plaintext()>
         
         auto lexer = Lexer(to_parse);
         size_t pos = 0;
-        while(auto token = lexer.next()) {
+        while(auto token = lexer.next_character()) {
             if (token->type == Lexer::TokenType::PlainText) {
                 std::copy_n(to_parse.text+token->begin, token->length(), this->text+pos);
                 pos += token->length();
@@ -117,3 +175,5 @@ struct Format : strlit::details::BaseString<Parser{to_parse}.length_plaintext()>
         }
     }
 };
+
+#endif
--- src/test.cpp
+++ src/test.cpp
@@ -2,19 +2,21 @@
 #include <cstfmt/parser.h>
 
 using namespace std;
-static constexpr char txt[] = "Hello {{'{1234}' world '{12}'}}!";
-static constexpr auto parser = Parser{txt};
+static constexpr char txt[] = "Hello {1:ab{2}cd} world";
+// static constexpr auto parser = Parser{txt};
 
-static constexpr auto length_plaintext = parser.length_plaintext();
-static constexpr auto number_arguments = parser.number_arguments();
-static constexpr auto formatted = Format<txt>{};
+// static constexpr auto length_plaintext = parser.length_plaintext();
+// static constexpr auto number_arguments = parser.number_arguments();
+// static constexpr auto formatted = Format<txt>{};
 
 // static_assert(parser.size_text() == 18);
 int main(int argc, char** argv)
 {
-    cout << "value:'" << formatted.sv() << "'" << endl;
-    cout << "length_plaintext:" << length_plaintext << endl;
-    cout << "number_arguments:" << number_arguments << endl;
+    // cout << "value:'" << formatted.sv() << "'" << endl;
+    // cout << "length_plaintext:" << length_plaintext << endl;
+    // cout << "number_arguments:" << number_arguments << endl;
+    auto lexer = Lexer(txt);
+    auto t = lexer.build_tree();
 
     return 0;
 }
