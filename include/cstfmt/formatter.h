e0b7753c65052a8ef27614e4de2c34f6a1702b5a
Gly <gabin.lefranc@gmail.com>
2022-12-11 19:37:58 +0100

    formatter move parser into parse func

--- include/cstfmt/formatter.h
+++ include/cstfmt/formatter.h
@@ -16,36 +16,36 @@ struct Formatter<StringT> {
     uint32_t width = 0;
     constexpr Formatter() = default;
     constexpr Formatter(std::string_view format_text) {
-        std::optional<char> align;
-        for (auto ch : format_text) {
+        parse(format_text);
+    }
+
+    constexpr auto parse(std::string_view input) -> std::string_view {
+        for (auto ch : input) {
             if (ch >= '0' && ch <= '9') {
                 fill = fill*10 + (ch-'0');
             } else if (ch == '<' || ch == '>' || ch == '^') {
-                this->align = ch;
+                align = ch;
             } else if (ch == '+' || ch == '-' || ch == ' ') {
                 sign = ch;
             }
         }
+        return std::string_view{};
     }
 
     constexpr auto format(const StringT& input) const -> std::string{
         auto str = std::string_view(input);
         std::string result;
         result.reserve(std::max<size_t>(str.length(),fill));
-        if (align == '<') {
+        if (str.length() <= fill) {
             result.append(str);
-            if (str.length() < fill) {
-                result.append(fill-str.length(), sign);
-            }
-        } else if (align == '>') {
-            if (str.length() < fill) {
-                result.append(fill-str.length(), sign);
-            }
-            result.append(str);
-        } else if (align == '^') {
-            if (str.length() >= fill) {
+        } else {
+            if (align == '<') {
                 result.append(str);
-            } else {
+                result.append(fill-str.length(), sign);
+            } else if (align == '>') {
+                result.append(fill-str.length(), sign);
+                result.append(str);
+            } else if (align == '^') {
                 auto left = std::max<int32_t>(0,fill-str.length())/2;
                 auto right = fill-str.length()-left;
                 result.append(left, sign);
@@ -61,8 +61,10 @@ template <std::integral IntegralT>
 struct Formatter<IntegralT> : Formatter<std::string_view>
 {
     constexpr Formatter() = default;
-    constexpr Formatter(std::string_view format_text) : Formatter<std::string_view>(format_text) 
-    {}
+    constexpr Formatter(std::string_view format_text) : Formatter<std::string_view>() 
+    {
+        Formatter<std::string_view>::parse(format_text); 
+    }
     constexpr auto format(IntegralT input) const -> std::string {
         return Formatter<std::string_view>::format(cfmt::utils::int_to_string(input));
     }
