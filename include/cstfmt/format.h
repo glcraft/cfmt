1044b2a0947440c631541ece560cf7525ad06c8c
Gly <gabin.lefranc@gmail.com>
2022-12-11 19:26:57 +0100

    add namespace cfmt in format

--- include/cstfmt/format.h
+++ include/cstfmt/format.h
@@ -8,109 +8,111 @@
 #include "utils.h"
 #include "formatter.h"
 
-
-struct Token {
-    int64_t id=-1;
-    std::string format;
-    constexpr Token(int64_t id, std::string_view format) : id(id), format(format) {}
-    constexpr Token(std::string_view format) : format(format) {}
-    constexpr Token(std::string&& format) : format(std::move(format)) {}
-    constexpr auto  is_arg() const {
-        return id != -1;
-    }
-};
-constexpr auto parse(std::string_view text) {
-    std::vector<Token> result;
-    int32_t level=0;
-    size_t prev_pos = 0;
-    uint32_t current_index = 0;
-    auto get_index = [&](std::optional<std::string_view> str) {
-        if (str && !str->empty()) {
-            return cfmt::utils::sv_to_int(*str);
+namespace cfmt 
+{
+    struct Token {
+        int64_t id=-1;
+        std::string format;
+        constexpr Token(int64_t id, std::string_view format) : id(id), format(format) {}
+        constexpr Token(std::string_view format) : format(format) {}
+        constexpr Token(std::string&& format) : format(std::move(format)) {}
+        constexpr auto  is_arg() const {
+            return id != -1;
         }
-        return static_cast<int64_t>(current_index++);
     };
-    auto add_string_to_last = [&](auto... to_add) {
-        if (result.empty()) {
-            result.emplace_back(std::string(to_add...));
-        }
-        auto& last = result.back();
-        last.format.append(to_add...);
-    };
-    for(size_t pos = 0;pos<text.length();pos++) {
-        auto ch = text[pos];
-        if (ch == '{') {
-            if (level == 0) {
-                if (pos < text.length()-1 && text[pos+1] == '{') {
-                    add_string_to_last('{', 1);
+    constexpr auto parse(std::string_view text) {
+        std::vector<Token> result;
+        int32_t level=0;
+        size_t prev_pos = 0;
+        uint32_t current_index = 0;
+        auto get_index = [&](std::optional<std::string_view> str) {
+            if (str && !str->empty()) {
+                return cfmt::utils::sv_to_int(*str);
+            }
+            return static_cast<int64_t>(current_index++);
+        };
+        auto add_string_to_last = [&](auto... to_add) {
+            if (result.empty()) {
+                result.emplace_back(std::string(to_add...));
+            }
+            auto& last = result.back();
+            last.format.append(to_add...);
+        };
+        for(size_t pos = 0;pos<text.length();pos++) {
+            auto ch = text[pos];
+            if (ch == '{') {
+                if (level == 0) {
+                    if (pos < text.length()-1 && text[pos+1] == '{') {
+                        add_string_to_last('{', 1);
+                        continue;
+                    }
+                    result.emplace_back(std::move(text.substr(prev_pos, pos-prev_pos)));
+                }
+                level++;
+                prev_pos = pos+1;
+            } else if (ch == '}') {
+                if (level == 0 && pos < text.length()-1 && text[pos+1] != '}') {
+                    add_string_to_last('}', 1);
                     continue;
                 }
-                result.emplace_back(std::move(text.substr(prev_pos, pos-prev_pos)));
-            }
-            level++;
-            prev_pos = pos+1;
-        } else if (ch == '}') {
-            if (level == 0 && pos < text.length()-1 && text[pos+1] != '}') {
-                add_string_to_last('}', 1);
-                continue;
-            }
-            level--;
-            if (level == 0) {
-                auto arg = text.substr(prev_pos, pos-prev_pos);
-                auto pos = arg.find(':');
-                if (pos == std::string::npos) {
-                    result.push_back(Token{get_index(std::nullopt), std::string()});
-                } else {
-                    result.push_back(Token{get_index(arg.substr(0, pos)), std::string(arg.substr(pos+1))});
+                level--;
+                if (level == 0) {
+                    auto arg = text.substr(prev_pos, pos-prev_pos);
+                    auto pos = arg.find(':');
+                    if (pos == std::string::npos) {
+                        result.push_back(Token{get_index(std::nullopt), std::string()});
+                    } else {
+                        result.push_back(Token{get_index(arg.substr(0, pos)), std::string(arg.substr(pos+1))});
+                    }
+                } else if (level < 0) {
+                    // throw std::runtime_error("Unmatched '}'");
                 }
-            } else if (level < 0) {
-                // throw std::runtime_error("Unmatched '}'");
+                prev_pos = pos+1;
             }
-            prev_pos = pos+1;
         }
-    }
-    if (prev_pos != text.length()) {
-        result.push_back(std::string(text.substr(prev_pos)));
-    }
-    return result;
-}
-constexpr auto format_string(std::string_view format_text, auto... args) -> std::string; 
-
-template <size_t N = 10000>
-constexpr auto format(std::string_view format_text, auto... args) -> strlit::String<N>
-{
-    strlit::String<N> res;
-    auto formatted = format_string(format_text, args...);
-    std::copy(formatted.begin(), formatted.end(), res.text);
-    std::fill(res.text+formatted.length(), res.text+res.size, '\0');
-    return res;
-}
-
-constexpr auto format_string(std::string_view format_text, auto... args) -> std::string {
-    auto parsed = parse(format_text);
-    std::string result;
-    for (auto& arg : parsed) {
-        if (!arg.is_arg()) {
-            result.append(arg.format);
-        } else {
-            // if (arg.id >= sizeof...(args)) {
-                // throw std::runtime_error("Not enough arguments");
-            // }
-            arg.format = format(std::string_view{arg.format}, args...);
-            int i=0;
-            ([&result, &arg, &i](auto&& arg_value) mutable {
-                if (i++ == arg.id) {
-                    result.append(Formatter<std::remove_cvref_t<decltype(arg_value)>>{arg.format}.format(arg_value));
-                    return true;
-                }
-                return false;
-            }(args) || ...);
+        if (prev_pos != text.length()) {
+            result.push_back(std::string(text.substr(prev_pos)));
         }
+        return result;
     }
-    return result;
-}
+    constexpr auto format_string(std::string_view format_text, auto... args) -> std::string; 
 
-static constexpr size_t format_length(std::string_view format_text, auto... args) 
-{
-    return format(format_text, args...).length();
+    template <size_t N = 10000>
+    constexpr auto format(std::string_view format_text, auto... args) -> strlit::String<N>
+    {
+        strlit::String<N> res;
+        auto formatted = format_string(format_text, args...);
+        std::copy(formatted.begin(), formatted.end(), res.text);
+        std::fill(res.text+formatted.length(), res.text+res.size, '\0');
+        return res;
+    }
+
+    constexpr auto format_string(std::string_view format_text, auto... args) -> std::string {
+        auto parsed = parse(format_text);
+        std::string result;
+        for (auto& arg : parsed) {
+            if (!arg.is_arg()) {
+                result.append(arg.format);
+            } else {
+                // if (arg.id >= sizeof...(args)) {
+                    // throw std::runtime_error("Not enough arguments");
+                // }
+                arg.format = format(std::string_view{arg.format}, args...);
+                int i=0;
+                ([&result, &arg, &i](auto&& arg_value) mutable {
+                    if (i++ == arg.id) {
+                        result.append(Formatter<std::remove_cvref_t<decltype(arg_value)>>{arg.format}.format(arg_value));
+                        return true;
+                    }
+                    return false;
+                }(args) || ...);
+            }
+        }
+        return result;
+    }
+
+    static constexpr size_t format_length(std::string_view format_text, auto... args) 
+    {
+        return format(format_text, args...).length();
+    }
 }
