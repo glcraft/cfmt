94c790f1d521195d04744fe3bb534f08d71003cd
Gly <gabin.lefranc@gmail.com>
2022-12-19 00:39:46 +0100

    update example

--- src/test.cpp
+++ src/test.cpp
@@ -7,14 +7,13 @@
 using namespace std;
 static constexpr char txt[] = "Hello {:-^20} <{:#x}> |{}| world";
 static constexpr auto integer = 123456;
-static constexpr auto flotant = 123.456;
 
-static constexpr auto formatted = strlit::Shrink<cfmt::format(txt, "foo", integer, flotant)>{};
+static constexpr auto formatted = strlit::Shrink<cfmt::format(txt, "foo", integer)>{};
 
 int main(int argc, char** argv)
 {
     cout << "value constexpr:'" << static_cast<std::string_view>(formatted) << "'" << endl;
-    auto test = cfmt::format(txt, "foo", integer, flotant);
+    auto test = cfmt::format(txt, "foo", integer);
     cout << "value runtime:'" << test.sv() << "'" << endl;
     
     return 0;
