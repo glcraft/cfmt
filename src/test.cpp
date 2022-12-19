51421509e60cd8af962a617aa121a836702e2161
Gly <gabin.lefranc@gmail.com>
2022-12-19 01:49:19 +0100

    update example

--- src/test.cpp
+++ src/test.cpp
@@ -1,19 +1,23 @@
 #include <cstddef>
 #include <iostream>
 #include <cfmt/format.h>
+#include <cfmt/formatter_container.h>
+#include <cfmt/formatter_map.h>
 #include <string_view>
 #include <tuple>
 
-using namespace std;
-static constexpr char txt[] = "Hello {:-^20} <{:-^#20x}> |{}| world";
-static constexpr auto integer = std::array{1,2,3};
 
-static constexpr auto formatted = strlit::Shrink<cfmt::format(txt, "foo", integer)>{};
+using namespace std;
+static constexpr char txt[] = "Hello {:-<30} <{:-^#20x}> world";
+static constexpr auto contained = std::array{std::array{1,2,3}, std::array{4,5,6}};
+static constexpr auto mapped = std::array{std::pair{"hello", 123}, std::pair{"world", 456}};
+
+static constexpr auto formatted = strlit::Shrink<cfmt::format(txt, contained, mapped)>{};
 
 int main(int argc, char** argv)
 {
     cout << "value constexpr:'" << static_cast<std::string_view>(formatted) << "'" << endl;
-    auto test = cfmt::format(txt, "foo", integer);
+    auto test = cfmt::format(txt, contained, mapped);
     cout << "value runtime:'" << test.sv() << "'" << endl;
     
     return 0;
